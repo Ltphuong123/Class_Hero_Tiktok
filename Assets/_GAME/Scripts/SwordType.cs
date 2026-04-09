1880c6430f6a0f430175d208959853de1adc8e0a
Ltphuong123 <101852175+Ltphuong123@users.noreply.github.com>
2026-04-09 17:41:05 +0700

    d

--- Assets/_GAME/Scripts/SwordType.cs
+++ Assets/_GAME/Scripts/SwordType.cs
@@ -1,11 +1,11 @@
 public enum SwordType
 {
-    Default,
-    Craze,
-    Fire,
-    Huge,
-    Iron,
-    Lightning,
-    Miasma,
-    Snow
+    Default = 0,
+    Craze = 1,
+    Fire = 2,
+    Huge = 3,
+    Iron = 4,
+    Lightning = 5,
+    Miasma = 6,
+    Snow = 7
 }
